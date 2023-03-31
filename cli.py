31bdb1b5c2a12d6fbd92d7da70873bafcf2b5e74
SivaPrasad43 <39110552+SivaPrasad43@users.noreply.github.com>
2023-03-31 12:03:19 +0530

    cli-ui:modified

--- cli.py
+++ cli.py
@@ -3,51 +3,70 @@ import random
 from timeit import default_timer as timer
 import time
 import os.path
+from rich.box import Box
+from rich.text import Text
+from rich.console import Console
+from rich.theme import Theme
+from rich.table import Table
+
+
+theme = Theme({"correct":"bold green","wrong":"bold red"})
+console = Console(theme=theme)
+
 
 data = pd.read_csv("data_added.csv")
 options = ["A","B","C","D"]
 beginner_count=intermediate_count=advanced_count = 0
 point = 0 
-
+question_array = []
 
 df = pd.DataFrame(columns=['qid', 'Answer', 'Selected Answer', 'Time Taken', 'Mark Obtained', 'Level'])
 df.to_csv('report.csv', index=False, mode='w')
 
-def typing_display(text, delay=0.1):
+def typing_display(text, delay=0):
     for char in text:
         print(char, end='', flush=True)
         time.sleep(delay)
     print()
 
 def program_exp():
-    user_ans = input("Do you have programing experience?(1=yes and 0=No)")
+    user_ans = input("Do you have programing experience?(1=yes and 0=No)\nChoose option: ")
+    print()
     return user_ans
 
 def run_quiz():
-    print("---PRE-ASSESSMENT TEST----\n")
-    print("Subject : PYTHON")
+    console.print("[b]---PRE-ASSESSMENT TEST----[/b]", justify="center", style="bold")
+    console.print("[b]Subject : PYTHON[/b]", justify="center", style="bold blue")
     response = program_exp()
     for count in range(15):
         question_set = question(count)
         selection_set =  select_answer()
         check_answer(question_set,selection_set,response)
+    print(question_array)
+    proficiency_prediction()
 
 def select_answer():
     starting_time = timer()
     answer = input("select option : ").upper()
+    print("\n")
     end_time = timer()
     time_taken = end_time - starting_time
     return answer,time_taken
 
 def question(count):
-    print("--------------------------------------")
-    random_index = random.choice(data.index)
-    random_row = data.loc[random_index]
-    question_id = random_row['Question ID']
+    global beginner_count,intermediate_count,advanced_count
+    while True:
+        random_index = random.choice(data.index)
+        random_row = data.loc[random_index]
+        question_id = random_row['Question ID']
+        if question_id not in question_array:
+            question_array.append(question_id)
+            break
     question = random_row['Question']
     answer = random_row['Correct Answer']
     level = random_row['Difficulty Level']
-    global beginner_count,intermediate_count,advanced_count
+
+    
     if level == "Beginner" and beginner_count<5:
         typing_display(f"{count+1} - {question}")
         beginner_count+=1
@@ -55,26 +74,32 @@ def question(count):
         typing_display(f"{count+1} - {question}")
         intermediate_count+=1
     elif level == "Advanced" and advanced_count<5:
-        typing_display(f"{count+1} - {question}")   
+        typing_display(f"{count+1} - {question}")  
         advanced_count+=1
+
     for i, op in enumerate(random_row[options]):
         print(f"{options[i]}) {op}", end="\n")
     print("--------------------------------------")
-    print(f"status => big : {beginner_count} inter : {intermediate_count} adv: {advanced_count}\n")
     return question_id,question,answer,level
 
 def check_answer(question_set,selection_set,response):
     global point,status
     if selection_set[0] == question_set[2]:
-        print("Correct!!")
+        message = Text("Correct!!", style="correct")
         point = mark_obtained(question_set[3])
         status = "correct"
     else:
-        print(f"Wrong!!")
+        message = Text("Wrong!!", style="wrong")
         status = "wrong"
+    console.print()
+    console.print("[b]Question Analysis[/b]", justify="center", style="bold")
+    console.print()
+    console.print(message, justify="center")
+    console.print()
     generate_analysis_report(question_set,selection_set,point,status,response)
-    print(f"Question Level : {question_set[3]}")
-    print(f"Time Taken : {'{:.2f}'.format(selection_set[1])} sec")
+    console.print(f"Question Level: {question_set[3]}", justify="center")
+    console.print(f"Time Taken: {'{:.2f}'.format(selection_set[1])} sec", justify="center")
+    console.print()
     point = 0
 
 def mark_obtained(level):
@@ -97,13 +122,20 @@ def generate_analysis_report(question_set,selection_set,point,status,response):
     'Level' : question_set[3] 
     }
     new_row = pd.DataFrame([analysis_data])
-    if os.path.isfile('report.csv'):
-        df = pd.read_csv('report.csv')
-        df = df.append(new_row, ignore_index=True)
-        df.to_csv('report.csv', index=False)
+    csv_file = 'report.csv'
+
+    if os.path.isfile(csv_file):
+        df = pd.concat([pd.read_csv('report.csv'), new_row], ignore_index=True)
+        df.to_csv(csv_file, index=False)
     else:
-        df = pd.DataFrame([analysis_data])
-        df.to_csv('report.csv', index=False)
+        df = pd.DataFrame([new_row])
+        df.to_csv(csv_file, index=False)
+
+
+def proficiency_prediction():
+    with open('gen.py', 'r') as f:
+        code_to_execute = f.read()
+    exec(code_to_execute)
 
 if __name__ == "__main__":
     run_quiz()
--- gen.py
+++ gen.py
@@ -1,34 +1,33 @@
 import pandas as pd
+from rich.console import Console
+from rich.theme import Theme
+from rich.table import Table
 
-# read pre-test report from CSV file
+console = Console()
+
 pretest_report = pd.read_csv('report.csv')
 
-# define weights and time thresholds for each difficulty level
 difficulty_weights = {'Beginner': 3, 'Intermediate': 5, 'Advanced': 8}
 time_thresholds = {'Beginner': 10, 'Intermediate': 20, 'Advanced': 30}
 penalty_factor = 1
 
-# set proficiency levels and minimum score thresholds
 proficiency_levels = ['Beginner', 'Intermediate', 'Advanced']
 min_scores = [0, 30, 60]
 
-# initialize total score and elapsed time
 total_score = 0
 elapsed_time = 0
 correct_ans = 0
 adv_q = 0
 intr_q =0
 big_q = 0
-# iterate over each row in the pre-test report
+
 for index, row in pretest_report.iterrows():
-    # extract relevant fields from the row
     user_answer = row['Selected Answer']
     correct_answer = row['Answer']
     time_taken = row['Time Taken']
     difficulty_level = row['Level']
     status = row['Status']
 
-    # calculate the score for the current question
     if status == "correct":
         correct_ans+=1
         if difficulty_level == "Beginner":
@@ -46,26 +45,29 @@ for index, row in pretest_report.iterrows():
     else:
         score = 0
 
-    # add the score to the total score
     total_score += score
-
-    # add the elapsed time for the current question
     elapsed_time += time_taken
 
-# calculate the index of the proficiency level based on the score and minimum score thresholds
 level_index = -1
 for i in range(len(proficiency_levels)):
     if total_score >= min_scores[i]:
         level_index = i
 
-# ensure the index is within valid bounds
 if level_index == -1:
     proficiency_level = 'No proficiency level'
 else:
     proficiency_level = proficiency_levels[level_index]
 
-# output the determined proficiency level
-print("Your score is "+ str(total_score))
-print(f"You are Answered {correct_ans} questions Correctly!!\n")
-print(f"They are Beginner:{big_q} Intermediate:{intr_q} Advanced:{adv_q}\n")
-print('The student belongs to', proficiency_level, 'proficiency level\n')
+table = Table(title="Student Score Report")
+table.add_column("Total Score")
+table.add_column("Correct Answers")
+table.add_column("Beginner")
+table.add_column("Intermediate")
+table.add_column("Advanced")
+table.add_column("Proficiency Level")
+table.add_row(str(total_score), str(correct_ans), str(big_q), str(intr_q), str(adv_q), proficiency_level)
+console.print(table, justify='center')
+# print("Your score is "+ str(total_score))
+# print(f"You are Answered {correct_ans} questions Correctly!!\n")
+# print(f"They are Beginner:{big_q} Intermediate:{intr_q} Advanced:{adv_q}\n")
+# print('The student belongs to', proficiency_level, 'proficiency level\n')
